5de80aec269714319f17553c42a8eefdfccdcd61
bahman bazoo <bahmanebazoo@gmail.com>
2019-06-08 15:45:06 +0430

    first page view created for check internet

--- app/src/main/java/com/maktab/maktabmarketmvvm/MainActivity.java
+++ app/src/main/java/com/maktab/maktabmarketmvvm/MainActivity.java
@@ -2,13 +2,33 @@ package com.maktab.maktabmarketmvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewBrand;
+    private TextView mTextViewads;
+    private ProgressBar mProgressBarCheckNet;
+
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context,MainActivity.class);
+
+        return intent;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgressBarCheckNet=findViewById(R.id.idWaitForNet);
+        mTextViewBrand=findViewById(R.id.idBrandName);
+        mTextViewads = findViewById(R.id.idAds);
+
     }
 }
